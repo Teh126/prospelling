b2e0af607be930d411685e697d1ecb799c33e51b
Teh126 <yvettekitto@gmail.com>
2017-10-13 11:05:41 +0100

    homepage new grid layout

--- src/HomeContainer.js
+++ src/HomeContainer.js
@@ -14,17 +14,21 @@ render(){
            </div>
   <div className="row">
     <div className="col">
-      1 of 3
+      Vowels
     </div>
     <div className="col">
-      2 of 3
+      Consonants
+    </div>
+    </div>
+    <div className="row">
+    <div className="col">
+      Letter NAME
     </div>
     <div className="col">
-      3 of 3
-    </div>
-    </div>
-    </div>
-    
+      Letter sounds
+      </div>
+   </div>
+   </div>
       
 
         )
